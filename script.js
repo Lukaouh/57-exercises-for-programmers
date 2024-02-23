9253d8dd32bfb8703584e6b1c6105fbecfec64f7
Luka <115785373+Lukaouh@users.noreply.github.com>
2024-02-23 19:09:43 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -70,7 +70,7 @@
 //     let Times = document.getElementById('times');
 //     let Div = document.getElementById('div');
 
-//     let Int = parseInt(firstNum);
+//     let Int = seInt(firstNum);
 //     let Int2 = parseInt(secondNum);
 //     if(Int < 0 || Int2 < 0){
 //         window.alert('Please enter positive numbers')
@@ -124,7 +124,7 @@
 
 // }
 
-
+//Area of a rectangular room 7th challeng of book;
 // function rectangle(){
 //     let Width = parseInt(document.getElementById('width').value);
 //     let Height = parseInt(document.getElementById('height').value);
@@ -188,3 +188,83 @@
 //     displayValue1.textContent = sessionStorage.getItem('Meters')
 // }
 // getMeters();
+
+//Pizza party 8th challeng of the book;
+// pizza.addEventListener('input', function() {
+//     const People = document.getElementById('people').value;
+//     const Pizza = document.getElementById('pizza').value;  
+//     const DisplayPeople = document.getElementById('displayPeople');
+//     //each pizzas have 8 pieces !!!!
+//     const Num = 8;
+//     //WholeNum gets us rounded number of pieces of pizza;
+//     const result = ((Pizza * Num) / People);
+//     const wholeNum = Math.floor(result);
+//     if(People < 0 || Pizza < 0){
+//         window.alert('Please enter real number of People and Pizza')
+//     }else{ 
+//     DisplayPeople.textContent = (`${People} people with ${Pizza} pizzas
+//     Each person gets ${wholeNum} piecies of pizza,
+//     There are ${(Pizza * Num) - (wholeNum * People)} leftover pieces.`);
+
+//     }
+// });
+
+
+//9th task of the book!! Paint Calculator!!!
+// height.addEventListener('input', function(){
+//     const Width = parseInt(document.getElementById('width').value);
+//     const Height = parseInt(document.getElementById('height').value);
+//     //1gallon cover 350feet square.
+//     const Gallon = 350 ;
+//     const Paragraph = document.getElementById('para');
+//     //Math.ceil round up numbers// example: it will round 3.14 to 4//
+//     const finallResult = Math.ceil((Width * Height) / Gallon)
+//     if(Width < 0 || Height < 0){
+//         window.alert('Please enter real size of the room')
+
+//     }else{
+//         Paragraph.innerHTML = (`You will need to purchase ${finallResult} gallons of paint to cover ${Width * Height} square feet`);
+//         saveDate();
+//     }
+
+// })
+// function saveDate(){
+//     sessionStorage.setItem('info', para.innerHTML);
+// }
+// function getItem(){
+//     para.innerHTML = sessionStorage.getItem('info');
+// }
+// getItem();
+
+
+//Self-Checkout 10th challeng of the book;
+// quanty_3.addEventListener('input', function(){
+//     let Item1 = parseInt(document.getElementById('item_1').value);
+//     let Quanty1 = parseInt(document.getElementById('quanty_1').value);
+//     let Item2 = parseInt(document.getElementById('item_2').value);
+//     let Quanty2 = parseInt(document.getElementById('quanty_2').value);
+//     let Item3 = parseInt(document.getElementById('item_3').value);
+//     let Quanty3 = parseInt(document.getElementById('quanty_3').value);
+//     //In this Task tax rate is 5.5% global;
+//     const Per = 5.5;
+   
+//         const Sub = document.getElementById('sub')
+//         .innerHTML = ((Item1 * Quanty1) + (Item2 * Quanty2) + (Item3 * Quanty3));
+    
+//         const Tax = document.getElementById('tax')
+//         .innerHTML = ((Sub * Per) / 100);
+    
+//         const Total= document.getElementById('total')
+//         .innerHTML = ( Sub + Tax);
+   
+  
+// })
+//Task 11th of the book; Currency Conversion
+// euro.addEventListener('input', function(){
+//     const Euro = document.getElementById('euro').value;
+//     const Value = (Euro * 2.7770) / 2.6850;
+//     const RoundedNumber = Value.toFixed(1);
+
+//     const Result = document.getElementById('result')
+//     .textContent = ('you convert euro to dollar' + RoundedNumber ) 
+// })
